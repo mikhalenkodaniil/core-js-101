da73404d01e6555df197edb501be3cc8e6e2423f
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-07-03 19:45:36 +0200

    feat(06-1,2,3):complete 1,2,3 06-promises.tasks.js

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,21 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise(((resolve, reject) => {
+    if (isPositiveAnswer === undefined) {
+      const err = 'Wrong parameter is passed! Ask her again.';
+      reject(Error(err));
+    } else {
+      let answer = '';
+      if (isPositiveAnswer) {
+        answer = 'Hooray!!! She said "Yes"!';
+      } else {
+        answer = 'Oh no, she said "No".';
+      }
+      resolve(answer);
+    }
+  }));
 }
 
 
@@ -48,8 +61,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +84,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
